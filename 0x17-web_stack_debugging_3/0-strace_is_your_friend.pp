f1fb393f01b929ad847ed44d580c4891589f6515
Endy4elect <endy4elect@yahoo.com>
2023-05-10 19:25:00 +0000

    corrections to typo error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,7 @@
-# Create a manifest that fixes file name typo
-exec { 'fix_typo':
-  command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
-  path    => '/bin/'
+# Configure server and make corrections to typo error
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
