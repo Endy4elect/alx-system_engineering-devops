6e5db134a95e8ba2ba5552d63bcb39a55d27fe22
Endy4elect <endy4elect@yahoo.com>
2023-05-18 18:09:05 +0000

    OS changing script enabling error free User login

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Puppet script that changes OS and gives user holbertono login right and open file without errors
+
+exec { 'increase-hard-file-limit-for-user-holberton':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-user-holberton':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
